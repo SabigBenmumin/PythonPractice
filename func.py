095e99c5359ab64b907871374a68b9bef282a245
SabigBenmumin <totpophy14755@gmail.com>
2022-04-23 22:44:43 +0700

    update water function

--- func.py
+++ func.py
@@ -1,13 +1,23 @@
 def BMI(weight,height):
     bmi = weight / ((height/100)**2)
     return bmi
+def WT(weight):
+    #WVPD is water volume per day
+    WVPD = weight * 2.2 * 30/2
+    return WVPD
 
-print("what you what bro (bmi)")
+print("what you want use function bmi/b water/w")
 Ft_select = str(input(" : "))
 
-if Ft_select == "bmi":
+if Ft_select == "bmi"or Ft_select =="b":
     print("user what to use BMI function")
 
-weight = float(input("how much your weight = "))
-height = float(input("how your thall = "))
-print("your BMI is :",BMI(weight,height))
+    weight = float(input("how much your weight [kg]= "))
+    height = float(input("how your thall [cm]= "))
+    print("your BMI is :",BMI(weight,height))
+elif Ft_select == "water" or Ft_select == "w":
+    print("user what to use water function") 
+    weight = float(input("how much your weight [kg] = "))
+    print("The amount of water you should drink per day is",WT(weight),"mL")
+    
+print("end program")
